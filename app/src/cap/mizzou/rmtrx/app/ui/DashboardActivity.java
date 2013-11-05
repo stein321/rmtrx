fb135c7d2cf8d9f48e2a0faafb306725186032fa
Ben Stein <stein321@gmail.com>
2013-11-05 13:40:35 -0600

    Deleted worthless junk in Dashboard. Is much cleaner now.

--- app/src/cap/mizzou/rmtrx/app/ui/DashboardActivity.java
+++ app/src/cap/mizzou/rmtrx/app/ui/DashboardActivity.java
@@ -1,24 +1,14 @@
 package cap.mizzou.rmtrx.app.ui;
 
-import Models.Residence;
-import Models.User;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.util.Log;
-import cap.mizzou.rmtrx.app.DataAccess.ResidenceDataInterface;
 import cap.mizzou.rmtrx.app.R;
-import cap.mizzou.rmtrx.app.User_setup.UserInfo;
 import cap.mizzou.rmtrx.core.ui.BaseFragmentActivity;
 import com.actionbarsherlock.view.Menu;
 import com.squareup.otto.Subscribe;
-import retrofit.Callback;
-import retrofit.RestAdapter;
-import retrofit.RetrofitError;
-import retrofit.client.Response;
 
 /**
  * Created with IntelliJ IDEA.
@@ -28,15 +18,13 @@ import retrofit.client.Response;
  * To change this template use File | Settings | File Templates.
  */
 public class DashboardActivity extends BaseFragmentActivity {
-    private UserInfo userInfo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.merge_empty);
         getSupportActionBar().setTitle("Dashboard");
-        Context context=getApplicationContext();
-        userInfo=new UserInfo(context);
         FragmentManager fragmentManager = getSupportFragmentManager();
 
         Fragment homeFragment = fragmentManager.findFragmentByTag(HomeFragment.TAG);
