287b041d1381456ff1aa3c4060a779c5184f2235
Alec Tegels <astfz9@mail.missouri.edu>
2013-11-07 23:31:29 -0600

    Splash screen. Fixed theme not working

--- app/src/cap/mizzou/rmtrx/app/ui/SplashActivity.java
+++ app/src/cap/mizzou/rmtrx/app/ui/SplashActivity.java
@@ -7,5 +7,35 @@ package cap.mizzou.rmtrx.app.ui;
  * Time: 5:52 PM
  * To change this template use File | Settings | File Templates.
  */
-public class SplashActivity {
+
+import java.util.Timer;
+import java.util.TimerTask;
+import cap.mizzou.rmtrx.app.R;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+    private long splashTime = 5000; //5 seconds
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        TimerTask task = new TimerTask()
+        {
+            @Override
+            public void run() {
+                finish();
+                Intent mainIntent = new Intent().setClass(SplashActivity.this, HomeActivity.class);
+                startActivity(mainIntent);
+            }
+
+        };
+
+        Timer timer = new Timer();
+        timer.schedule(task, splashTime);
+    }
 }
